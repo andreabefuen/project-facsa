2030c834fdf5b08277ec89d24a0485473765441d
andreabefuen <al341488@uji.es>
2018-09-03 11:54:25 +0200

    BuildManager y otras cosas

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+
+    public static BuildManager instance;
+
+    private void Awake()
+    {
+
+        if (instance != null)
+        {
+            Debug.LogError("More than a one BuildManager in scene!");
+            return;
+        }
+        instance = this;
+
+     
+        
+    }
+
+    public GameObject standardEdificationPrefab;
+
+    private GameObject structureToBuild; 
+
+    public GameObject GetStructureToBuild()
+    {
+        return structureToBuild;
+    }
+
+	// Use this for initialization
+	void Start () {
+
+        structureToBuild = standardEdificationPrefab;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour {
+
+    public Color hoverColor;
+
+
+    public BuildManager build;
+
+    private GameObject edification;
+
+    private Renderer rend;
+    private Color startColor;
+
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+	}
+
+    private void OnMouseDown()
+    {
+        if(edification != null)
+        {
+            Debug.Log("Can't build there!"); //Display on screen
+            return;
+        }
+
+        //Build an edification
+
+
+        GameObject structureToBuild = BuildManager.instance.GetStructureToBuild();
+        edification = (GameObject)Instantiate(structureToBuild, transform.position, transform.rotation);
+
+    }
+
+    private void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material.color = Color.blue;
+        //rend.material.color = startColor;
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
