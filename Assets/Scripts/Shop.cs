64aff5a5127dcd5b44b844a084fc9751f5c9f5db
andreabefuen <al341488@uji.es>
2018-09-03 18:32:43 +0200

    Modelados nuevos

---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour {
+
+    public void PurchaseStandardStructure()
+    {
+        Debug.Log("Standard Structure Purchased");
+    }
+
+    public void PurchaseAnotherStructure()
+    {
+        Debug.Log("Another structure purchased");
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
