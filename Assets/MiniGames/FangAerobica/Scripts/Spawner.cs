3c74fbf7525f81fec86123190ab7d7b8b9bf248c
axel8890 <32980687+axel8890@users.noreply.github.com>
2018-09-03 13:44:23 +0200

    Add fangAerobica

---
+++ Assets/MiniGames/FangAerobica/Scripts/Spawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject[] bricks;
+    public int numOxygen = 13;
+    public GameObject[] bricksInstantiate;
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                Vector3 pos = new Vector3(-4.05f + j * 0.9f, 2.75f + i * 0.5f, 0); ;
+                Instantiate(bricks[i], pos, transform.rotation);
+
+            }
+        }
+        int oxy = 0;
+        bricksInstantiate = GameObject.FindGameObjectsWithTag("Brick");
+      
+        while (oxy < numOxygen) {
+            int Rdn = UnityEngine.Random.Range(0, bricksInstantiate.Length);
+            BrickScript brickScriptInstant = bricksInstantiate[Rdn].GetComponent<BrickScript>();
+            if (!brickScriptInstant.haveOxygen) {
+                brickScriptInstant.haveOxygen = true;
+                oxy += 1;
+            }
+        }
+        Destroy(gameObject);
+    }
+
+}
