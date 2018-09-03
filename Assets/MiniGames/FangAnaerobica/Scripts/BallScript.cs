24cd53afe93ff3cc78759e9f02720280b1075325
axel8890 <32980687+axel8890@users.noreply.github.com>
2018-09-03 13:00:30 +0200

    Add Fanganaerobica
    Añadido el juego

---
+++ Assets/MiniGames/FangAnaerobica/Scripts/BallScript.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+
+    public float explosionStrength = 1500;
+    AudioSource Sounds;
+    bool BallType;
+    Renderer rend;
+    AudioSource Sound;
+    public AudioClip Bell;
+    public AudioClip Fail;
+    // Use this for initialization
+    void Start () {
+        BallType = true;
+        Sound = GetComponent<AudioSource>();
+        rend = GetComponent<Renderer>();
+        rend.material.SetColor("_Color", new Color(0,255,0,255));
+        Sounds = gameObject.GetComponent<AudioSource>();
+
+	}
+    private void Update()
+    {
+        if (Input.GetButtonDown("BallType"))
+            ChangeBall();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Bactery")
+        {
+            if (collision.gameObject.name == "GoodBactery(Clone)")
+            {
+                if (BallType)
+                {
+                    ManagerScript.Score += 150;
+                    Sound.clip = Bell;
+                    Sound.Play();
+                }
+                else {
+                    ManagerScript.Score -= 100;
+                    Sound.clip = Fail;
+                    Sound.Play();
+                }
+                this.GetComponent<Rigidbody>().AddExplosionForce(explosionStrength, collision.transform.position, 5);
+                Destroy(collision.gameObject);
+                ManagerScript.NumBacteries -= 1;
+            }
+            if (collision.gameObject.name == "BadBactery(Clone)")
+            {
+                if (BallType)
+                {
+                    ManagerScript.Score -= 100;
+                    Sound.clip = Fail;
+                    Sound.Play();
+                }
+                else
+                {
+                    ManagerScript.Score += 150;
+                    Sound.clip = Bell;
+                    Sound.Play();
+                }
+                this.GetComponent<Rigidbody>().AddExplosionForce(explosionStrength, collision.transform.position, 5);
+                Destroy(collision.gameObject);
+                ManagerScript.NumBacteries -= 1;
+            }
+        }
+    }
+    void ChangeBall()
+    {
+        if (BallType)
+        {
+            BallType = false;
+            rend.material.SetColor("_Color", new Color(255, 0, 0, 255));
+        }
+        else
+        {
+            BallType = true;
+            rend.material.SetColor("_Color", new Color(0, 255, 0, 255));
+        }
+    }
+}
---
+++ Assets/MiniGames/FangAnaerobica/Scripts/ManagerScript.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManagerScript : MonoBehaviour {
+
+    public static int Score;
+    public static int Life;
+    public static int NumBacteries;
+    public static bool BallInGame;
+    public GameObject GoodBactery;
+    public GameObject BadBactery;
+    public GameObject GameOverObject;
+    public AudioSource GameOverSound;
+    int TimeToRespawn;
+    Text ScoreText;
+    Text LifeText;
+    GameObject[] Spawners;
+    bool finish;
+
+
+    // Use this for initialization
+    void Start () {
+        Physics.gravity = new Vector3(0, -4.4F, -11);
+        GameOverObject.SetActive(false);
+        TimeToRespawn = 0;
+        ScoreText = GameObject.Find("ScoreText").GetComponent<Text>();
+        LifeText = GameObject.Find("LifeText").GetComponent<Text>();
+        Spawners = GameObject.FindGameObjectsWithTag("Spawner");
+        Score = 0;
+        Life = 2;
+        TimeToRespawn = 300;
+        BallInGame = false;
+        ScoreText.text = "Puntuació\n" + Score.ToString("00000");
+        LifeText.text = "Boles\n " + Life;
+        finish = false;
+        NumBacteries = 0;
+        while (NumBacteries < 3)
+        {
+            CreateBactery();
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (BallInGame)
+        {
+            ScoreText.text = "Puntuació\n" + Score.ToString("00000");
+            LifeText.text = "Boles\n " + Life;
+            if (TimeToRespawn <= 0 || NumBacteries <3)
+            {
+                CreateBactery();
+                TimeToRespawn = 300;
+            }
+            else
+            {
+                TimeToRespawn -= 1;
+            }
+        }
+        if(Life < 0)
+        {
+            GameOver();
+        }
+    }
+    void GameOver()
+    {
+        Physics.gravity = new Vector3(0, -9.81f, 0);
+
+        ScoreText.text = "Puntuació\n" + Score.ToString("00000");
+        LifeText.text = "Boles\n 0";
+        BallInGame = false;
+        GameOverObject.SetActive(true);
+        if (!finish)
+        {
+            GameOverSound.Play();
+            finish = true;
+        }
+        Destroy(GameObject.Find("Ball"));
+        GameObject[] Bacteries = GameObject.FindGameObjectsWithTag("Bactery");
+        foreach (GameObject bactery in Bacteries)
+        {
+            Destroy(bactery);
+        }
+        GameObject[] Spawners = GameObject.FindGameObjectsWithTag("Spawner");
+        foreach (GameObject spawn in Spawners)
+        {
+            Destroy(spawn);
+        }
+
+    }
+    void CreateBactery()
+    {
+        int rdm = UnityEngine.Random.Range(0, Spawners.Length);
+        if (Spawners[rdm].transform.childCount <= 0)
+        {
+            int rdm2 = UnityEngine.Random.Range(0, 2);
+            if (rdm2 == 0)
+            {
+                GameObject Bactery = Instantiate(GoodBactery, Spawners[rdm].transform.position, Quaternion.identity);
+                Bactery.transform.parent = Spawners[rdm].transform;
+            }
+            else
+            {
+                GameObject Bactery = Instantiate(BadBactery, Spawners[rdm].transform.position, Quaternion.identity);
+                Bactery.transform.parent = Spawners[rdm].transform;
+            }
+            NumBacteries += 1;
+        }
+    }
+}
