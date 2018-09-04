843e899bd2a792a3813b836d548e6e7411bd8a0e
axel8890 <32980687+axel8890@users.noreply.github.com>
2018-09-04 12:36:14 +0200

    Add "Desbaste"
    Añadido el juego desbaste

---
+++ Assets/MiniGames/Desbaste/Scripts/DropZone_DB.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone_DB : MonoBehaviour, IDropHandler {
+
+	public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log(eventData.pointerDrag.name + " esta en " + gameObject.name);
+        Draggable_DB d = eventData.pointerDrag.GetComponent<Draggable_DB>();
+        Destroy(eventData.pointerDrag);
+        
+    }
+}
---
+++ Assets/MiniGames/Desbaste/Scripts/SpawnScript_DB.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript_DB : MonoBehaviour {
+
+    public GameObject[] Objects;
+    public int timeRespawn;
+    public float speed;
+
+
+    // Use this for initialization
+    private void Start()
+    {
+        speed = 2f;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (timeRespawn <= 0)
+        {
+            int Rdn = UnityEngine.Random.Range(0, Objects.Length);
+            GameObject newObject = Instantiate(Objects[Rdn], transform.position, transform.rotation);
+            TrashMovement_DB movement = newObject.GetComponent<TrashMovement_DB>();
+            movement.speed = speed;
+            timeRespawn = UnityEngine.Random.Range((int)(180/(speed/2)), (int)(300 / (speed / 2)));
+        }
+        else {
+            timeRespawn -= 1;
+        }
+    }
+}
