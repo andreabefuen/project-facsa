32af774311748c64414189acacdcaa2d35f95c92
axel8890 <32980687+axel8890@users.noreply.github.com>
2018-09-04 17:17:25 +0200

    Add "Tractament FQ"
    Añadido minijuego Tractament Fisic quimic.

---
+++ Assets/MiniGames/Tractament FQ/Scripts/ManagerScript_TFQ.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerScript_TFQ : MonoBehaviour {
+
+    public GameObject Element1;
+    public GameObject Element2;
+    public GameObject Panel;
+    public GameObject WinPanel;
+    public AudioClip WinSound;
+    AudioSource Music;
+    string[] elements;
+    int elemInArray;
+	// Use this for initialization
+	void Start () {
+        Music = GetComponent<AudioSource>();
+        elements = new string[20];
+        elemInArray = 0;
+        Spawn();
+
+    }
+    void Spawn()
+    {
+        int num_elem1 = 0;
+        int num_elem2 = 0;
+        for(int i = 0; i < 20; i++)
+        {
+            if(num_elem1 >= 10)
+            {
+                GameObject newElem = Instantiate(Element2, Element2.transform.position, Element2.transform.rotation);
+                newElem.transform.SetParent(Panel.transform);
+                newElem.GetComponent<SelectScript_TFQ>().pos = i;
+                num_elem2 += 1;
+            }
+            else if (num_elem2 >= 10)
+            {
+                GameObject newElem = Instantiate(Element1, Element1.transform.position, Element1.transform.rotation);
+                newElem.transform.SetParent(Panel.transform);
+                newElem.GetComponent<SelectScript_TFQ>().pos = i;
+                num_elem1 += 1;
+            }
+            else
+            {
+                int rand = UnityEngine.Random.Range(0, 2);
+                if(rand == 1)
+                {
+                    GameObject newElem = Instantiate(Element1, Element1.transform.position, Element1.transform.rotation);
+                    newElem.transform.SetParent(Panel.transform);
+                    newElem.GetComponent<SelectScript_TFQ>().pos = i;
+                    num_elem1 += 1;
+                }
+                else
+                {
+                    GameObject newElem = Instantiate(Element2, Element2.transform.position, Element2.transform.rotation);
+                    newElem.transform.SetParent(Panel.transform);
+                    newElem.GetComponent<SelectScript_TFQ>().pos = i;
+                    num_elem2 += 1;
+                }
+                
+            }
+        }
+
+    }
+
+    public void AddArray(int pos, string type) {
+
+        elements[pos] = type;
+        elemInArray++;
+        if (elemInArray == 10) {
+            Comprobar();
+        }
+    }
+    public void RemoveArray(int pos)
+    {
+        elements[pos] = null;
+        elemInArray--;
+        if (elemInArray == 10)
+        {
+            Comprobar();
+        }
+    }
+    void Comprobar()
+    {
+        Debug.Log("Comprobando");
+        int contador = 0;
+        for(int i = 0; i < 20; i++)
+        {
+
+            if (elements[i] == "Element1")
+            {
+                contador += 1;
+            }
+        }
+
+        if(contador == 10)
+        {
+            Win();
+        }
+    }
+    void Win()
+    {
+        WinPanel.SetActive(true);
+        Music.Stop();
+        Music.loop = false;
+        Music.clip = WinSound;
+        Music.Play();
+        GameObject[] elements = GameObject.FindGameObjectsWithTag("Element1");
+        foreach(GameObject element in elements)
+        {
+            Destroy(element);
+        }
+        elements = GameObject.FindGameObjectsWithTag("Element2");
+        foreach (GameObject element in elements)
+        {
+            Destroy(element);
+        }
+    }
+}
---
+++ Assets/MiniGames/Tractament FQ/Scripts/SelectScript_TFQ.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectScript_TFQ : MonoBehaviour {
+    public GameObject manager;
+    public Outline Aura;
+    public int pos;
+    public AudioClip SelecClip;
+    public AudioClip DeselecClip;
+    AudioSource Sound;
+    bool selected;
+    ManagerScript_TFQ managerSc;
+
+    // Use this for initialization
+
+    private void Start()
+    {
+        selected = false;
+        Sound = GetComponent<AudioSource>();
+        manager = GameObject.Find("Manager");
+        managerSc = manager.GetComponent<ManagerScript_TFQ>();
+    }
+    // Update is called once per frame
+    public void Select () {
+		if (!selected)
+        {
+            Sound.clip = SelecClip;
+            Sound.Play();
+            managerSc.AddArray(pos, gameObject.tag);
+            selected = true;
+            Aura.enabled = true;
+        }
+        else
+        {
+            Sound.clip = DeselecClip;
+            Sound.Play();
+            managerSc.RemoveArray(pos);
+            selected = false;
+            Aura.enabled = false;
+        }
+    
+	}
+}
