c074175d40017af5065064a4a7e00f8ed21f3fd5
axel8890 <32980687+axel8890@users.noreply.github.com>
2018-09-04 13:04:29 +0200

    Add "Desordenado-Desengrasado"
    Añadido minijuego Desordenado-Desengrasado y arreglar aireacio.

---
+++ Assets/MiniGames/Desordenado/Scripts/DropZone_DD.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone_DD : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag == null)
+            return;
+        
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag == null)
+            return;
+
+        Draggable_DD d = eventData.pointerDrag.GetComponent<Draggable_DD>();
+
+    }
+
+
+	public void OnDrop(PointerEventData eventData)
+    {
+        //Debug.Log(eventData.pointerDrag.name + " esta en " + gameObject.name);
+        Draggable_DD d = eventData.pointerDrag.GetComponent<Draggable_DD>();
+        if (d != null)
+        {
+            d.parentToReturnTo = this.transform;
+        }
+
+    }
+}
---
+++ Assets/MiniGames/Desordenado/Scripts/ProvetaScript_DD.cs
@@ -0,0 +1,190 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProvetaScript_DD : MonoBehaviour {
+
+    public GameObject GameOverObj;
+    public GameObject WinObj;
+    public GameObject Fail;
+    public GameObject first;
+    public GameObject second;
+    public GameObject third;
+    public GameObject fourth;
+    public GameObject fifth;
+    public AudioClip WinClip;
+    public AudioClip GameOverClip;
+    public Color WaterColor;
+    public Color OilColor;
+    public Color AlcoholColor;
+    public Color HoneyColor;
+    public Color SoapColor;
+    AudioSource Music;
+    SpriteRenderer firstRend;
+    SpriteRenderer SecondRend;
+    SpriteRenderer ThirdRend;
+    SpriteRenderer FourthRend;
+    SpriteRenderer FifthRend;
+    string[] ElementsInTube;
+    int numElements = 1;
+
+    // Use this for initialization
+    private void Start()
+    {
+        Music = GetComponent<AudioSource>();
+        Fail.SetActive(false);
+        WinObj.SetActive(false);
+        GameOverObj.SetActive(false);
+        ElementsInTube = new string[5];
+        firstRend = first.GetComponent<SpriteRenderer>();
+        SecondRend = second.GetComponent<SpriteRenderer>();
+        ThirdRend= third.GetComponent<SpriteRenderer>();
+        FourthRend = fourth.GetComponent<SpriteRenderer>();
+        FifthRend = fifth.GetComponent<SpriteRenderer>();
+
+    }
+
+    public void newElement(string element)
+    {
+        //Debug.Log(element);
+        switch (numElements)
+        {
+            case 1:
+                firstRend.color = WhatColor(element);
+                ElementsInTube[0] = element;
+                numElements++;
+                break;
+            case 2:
+                if (Comprobar(element))
+                {
+                    SecondRend.color = WhatColor(element);
+                    ElementsInTube[1] = element;
+                    numElements++;
+                }
+                else
+                {
+                    StartCoroutine(GameOver());
+                }
+                break;
+            case 3:
+                if (Comprobar(element))
+                {
+                    ThirdRend.color = WhatColor(element);
+                    ElementsInTube[2] = element;
+                    numElements++;
+                }
+                else
+                {
+                    StartCoroutine(GameOver());
+                }
+                break;
+            case 4:
+                if (Comprobar(element))
+                {
+                    Comprobar(element);
+                    FourthRend.color = WhatColor(element);
+                    ElementsInTube[3] = element;
+                    numElements++;
+                }
+                else
+                {
+                    StartCoroutine(GameOver());
+                }
+                break;
+            case 5:
+                if (Comprobar(element))
+                {
+                    FifthRend.color = WhatColor(element);
+                    ElementsInTube[4] = element;
+                    Win();
+                }
+                else
+                {
+                    StartCoroutine(GameOver());
+                }
+                break;
+        }
+        
+    }
+    Color WhatColor(string element)
+    {
+        if(element == "Oil") return OilColor;
+        else if (element == "Water") return WaterColor;
+        else if (element == "Soap") return SoapColor;
+        else if (element == "Alcohol") return AlcoholColor;
+        else if (element == "Honey") return HoneyColor;
+        else return new Color(0, 0, 0, 0);
+
+    }
+
+    bool Comprobar(string element)
+    {
+        if (ElementsInTube.Length > 0)
+        {
+            if (element == "Oil")
+            {
+                if (System.Array.IndexOf(ElementsInTube, "Alcohol") != -1)
+                {
+                    //Debug.Log("Desborda");
+                    return false;
+                }
+            }
+            else if (element == "Water")
+            {
+                if (System.Array.IndexOf(ElementsInTube, "Alcohol") != -1 || System.Array.IndexOf(ElementsInTube, "Oil") != -1)
+                {
+                    //Debug.Log("Desborda");
+                    return false;
+                }
+            }
+            else if (element == "Soap")
+            {
+                if (System.Array.IndexOf(ElementsInTube, "Alcohol") != -1 || System.Array.IndexOf(ElementsInTube, "Oil") != -1 || System.Array.IndexOf(ElementsInTube, "Water") != -1)
+                {
+                    //Debug.Log("Desborda");
+                    return false;
+                }
+            }
+            else if (element == "Honey")
+            {
+                if (System.Array.IndexOf(ElementsInTube, "Alcohol") != -1 || System.Array.IndexOf(ElementsInTube, "Oil") != -1 || System.Array.IndexOf(ElementsInTube, "Water") != -1 || System.Array.IndexOf(ElementsInTube, "Soap") != -1)
+                {
+                   // Debug.Log("Desborda");
+                    return false;
+                }
+            }
+        }
+        return true;
+        
+    }
+    IEnumerator GameOver()
+    {
+        Music.loop = false;
+        Music.clip = GameOverClip;
+        Music.Play();
+        GameObject[] Elements;
+        Elements = GameObject.FindGameObjectsWithTag("Objects");
+        foreach (GameObject objeto in Elements)
+        {
+            Destroy(objeto);
+        }
+        bool failActive = true;
+        for(int i = 0; i < 30; i++)
+        {
+            Fail.SetActive(failActive);
+            yield return new WaitForSeconds(0.1f);
+            failActive = !failActive;
+        }
+        
+        GameOverObj.SetActive(true);
+
+    }
+    void Win()
+    {
+        Music.loop = false;
+        Music.clip = WinClip;
+        Music.Play();
+        WinObj.SetActive(true);
+
+    }
+}
