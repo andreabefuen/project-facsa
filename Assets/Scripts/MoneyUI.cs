599c075d79c10621ec0f663b8185b52c8226c1b3
andreabefuen <al341488@uji.es>
2018-09-05 17:01:31 +0200

    Hud y más cosas

---
+++ Assets/Scripts/MoneyUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MoneyUI : MonoBehaviour {
+
+    //public TextMeshPro moneyText;
+    private TextMeshProUGUI moneyText;
+
+	// Use this for initialization
+	void Awake () {
+        moneyText = GetComponent<TextMeshProUGUI>();
+        moneyText.text = "Money";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moneyText.text = "Money: " + PlayerStats.Money.ToString() + " $";
+
+	}
+}
