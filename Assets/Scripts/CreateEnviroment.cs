1622c8f0aa5fc120b2ff9d40d09f467281780d8c
andreabefuen <al341488@uji.es>
2018-09-10 17:23:21 +0200

    ALGORITMO DE CREACIÓN DE GRAFO Y DE ESCENA

--- Assets/Scripts/CreateEnviroment.cs
+++ Assets/Scripts/CreateEnviroment.cs
@@ -9,9 +9,11 @@ public class CreateEnviroment : MonoBehaviour {
 
     public GameObject node;
 
-    private float heightNode, widthNode;
+    //private float heightNode, widthNode;
 
-    private Vector3 offset = new Vector3 (2,2,2);
+    private Vector3 aux = new Vector3 (1.5f,0,0);
+
+    private float offset = 1.5f;
 
     private GameObject[,] matrix;
     Vector3 position = new Vector3 (0, 0, 0);
@@ -19,8 +21,8 @@ public class CreateEnviroment : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-        heightNode = 5f;
-        widthNode = 5f;
+        //heightNode = 5f;
+       // widthNode = 5f;
 
         matrix = new GameObject[filas, columnas];
 
@@ -29,17 +31,117 @@ public class CreateEnviroment : MonoBehaviour {
 
             for (int c = 0; c < columnas; c++)
             {
-                transform.position = position;
+                node.transform.position = position;
+                
                 matrix[f, c] = Instantiate(node);
-                position += offset;
+                position += aux;
 
             }
+            position = new Vector3(0, 0, position.z + offset);
+
+            //offset = new Vector3(0, 0, 1.5f);
 
            // position = (widthNode, 0, 0);
         }
 
+
+        CreateGrafo();
+
     }
-	
+
+    public void CreateGrafo()
+    {
+
+
+        for (int f = 0; f < filas; f++)
+        {
+
+            for (int c = 0; c < columnas; c++)
+            {
+                matrix[f, c].GetComponent<Nodo>().positionText.text = "(" + f + "," + c + ")";
+
+                //ESQUINAS
+                if(f == 0 && c == 0)
+                {
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f + 1, 0].GetComponent<Nodo>());
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f , c+1].GetComponent<Nodo>());
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f + 1, c+1].GetComponent<Nodo>());
+
+                }
+                else if( f == filas-1 && c == columnas-1)
+                {
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f - 1, c].GetComponent<Nodo>());
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f, c - 1].GetComponent<Nodo>());
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f - 1, c - 1].GetComponent<Nodo>());
+                }
+
+                else if(f == filas -1 && c == 0)
+                {
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f , c+1].GetComponent<Nodo>());
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f-1, c+1 ].GetComponent<Nodo>());
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f - 1, c ].GetComponent<Nodo>());
+                }
+
+                else if(f == 0 && c == columnas - 1)
+                {
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[0 , c-1].GetComponent<Nodo>());
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[1, c -1].GetComponent<Nodo>());
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[1, c ].GetComponent<Nodo>());
+                }
+                //BORDES
+                else if( f == 0)
+                {
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f, c - 1].GetComponent<Nodo>());
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f, c + 1].GetComponent<Nodo>());
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f+1, c-1].GetComponent<Nodo>());
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f+1, c].GetComponent<Nodo>());
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f+1, c+1].GetComponent<Nodo>());
+                }
+
+                else if( c == 0)
+                {
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f+1, c ].GetComponent<Nodo>());
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f-1, c].GetComponent<Nodo>());
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f + 1, c + 1].GetComponent<Nodo>());
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f + 1, c].GetComponent<Nodo>());
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f - 1, c +1].GetComponent<Nodo>());
+                }
+
+                else if( f == filas -1)
+                {
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f, c - 1].GetComponent<Nodo>());
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f, c + 1].GetComponent<Nodo>());
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f - 1, c - 1].GetComponent<Nodo>());
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f - 1, c].GetComponent<Nodo>());
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f - 1, c + 1].GetComponent<Nodo>());
+
+                }
+                else if (c == columnas - 1)
+                {
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f+1 , c ].GetComponent<Nodo>());
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f -1 , c].GetComponent<Nodo>());
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f + 1, c-1 ].GetComponent<Nodo>());
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f , c-1].GetComponent<Nodo>());
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f - 1, c - 1].GetComponent<Nodo>());
+
+                }
+                //EN MEDIO
+                else
+                {
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f + 1, c-1].GetComponent<Nodo>());
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f , c-1].GetComponent<Nodo>());
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f - 1, c - 1].GetComponent<Nodo>());
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f + 1, c].GetComponent<Nodo>());
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f - 1, c ].GetComponent<Nodo>());
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f + 1, c+1].GetComponent<Nodo>());
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f , c+1].GetComponent<Nodo>());
+                    matrix[f, c].GetComponent<Nodo>().adyacentes.Add(matrix[f -1 , c + 1].GetComponent<Nodo>());
+                }
+
+            }
+        }
+    }
+
 	// Update is called once per frame
 	void Update () {
 		
---
+++ Assets/Scripts/Nodo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Nodo : MonoBehaviour {
+
+
+
+    public List<Nodo> adyacentes;
+
+    public Text positionText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
