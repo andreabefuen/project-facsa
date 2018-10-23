16427cc18d2f0d92713660ff40638e23e1fe9666
andreabefuen <al341488@uji.es>
2018-10-23 09:52:56 +0200

    cambios

---
+++ Assets/Water Drop/Water Drop 3D/Scripts/CameraMovement3D.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement3D : MonoBehaviour {
+
+    public float velocityX;
+
+	// Use this for initialization
+	void Start () {
+
+        velocityX *= -1;
+
+        GetComponent<Rigidbody>().velocity = new Vector3(velocityX, 0, 0);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
