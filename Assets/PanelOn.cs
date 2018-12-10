a42adad816a5c8fbd6c8797248222014d9449757
andreabefuen <al341488@uji.es>
2018-12-10 15:59:04 +0100

    Menú y cambios en niveles

---
+++ Assets/PanelOn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelOn : MonoBehaviour {
+
+    public GameObject panelWater;
+    public GameObject generalPanel;
+
+    public void OnButtonPulseWater()
+    {
+        panelWater.SetActive(true);
+        generalPanel.SetActive(false);
+        
+    }
+}
